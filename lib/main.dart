e2aae16b591438c6f54356cc6f4246d3ccb4cb20
Asmaathabet <asmathabet2009@hotmail.com>
2020-08-11 20:30:18 +0300

    add stateless widget to do Hot Reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,37 @@
 import 'package:flutter/material.dart';
-
+// property: widget
 void main() => runApp(MaterialApp (
-  // property: widget
- home: Scaffold(
-   appBar: AppBar(
-     title: Text('my first App'),
-     centerTitle: true,
-     backgroundColor: Colors.deepOrange[600],  // press ctrl + Q
-   ),
-   body: Center(
-     child: Text(
-     'Hello from Flutter',
-     style : TextStyle(
-       fontSize: 20.0,
-       fontWeight: FontWeight.bold,
-       letterSpacing: 0.2,
-       color: Colors.grey[600],
-       fontFamily: 'IndieFlower',
-     ),
-     ),
-     ),
-   floatingActionButton: FloatingActionButton(
-     tooltip: 'Add',
-     child: Icon(Icons.add),
-     onPressed: () {},
-     backgroundColor: Colors.deepOrange[600],
-   ),
- ),
+ home: Home(),
 ));
+// use shortcut stless
+class Home extends StatelessWidget {
+  // Hot Reload Action - build
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+    title: Text('first App'),
+    centerTitle: true,
+    backgroundColor: Colors.deepOrange[600],  // press ctrl + Q
+    ),
+    body: Center(
+    child: Text(
+    'This is Asmaa, Hello from Flutter',
+    style : TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 0.2,
+    color: Colors.grey[600],
+    fontFamily: 'IndieFlower',
+    ),
+    ),
+    ),
+    floatingActionButton: FloatingActionButton(
+    tooltip: 'Add',
+    child: Icon(Icons.add),
+onPressed: () {},
+backgroundColor: Colors.deepOrange[600],
+),
+);
+  }
+}
