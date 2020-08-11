f29a1ae98c3ae1f4a28bf2774c67a0d959b82533
Asmaathabet <asmathabet2009@hotmail.com>
2020-08-11 19:22:01 +0300

    change colors, fonts and some style

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,25 @@ void main() => runApp(MaterialApp (
    appBar: AppBar(
      title: Text('my first App'),
      centerTitle: true,
+     backgroundColor: Colors.deepOrange[600],  // press ctrl + Q
    ),
    body: Center(
-     child: Text('Hello from Flutter'),
- ),
+     child: Text(
+     'Hello from Flutter',
+     style : TextStyle(
+       fontSize: 20.0,
+       fontWeight: FontWeight.bold,
+       letterSpacing: 0.2,
+       color: Colors.grey[600],
+       fontFamily: 'IndieFlower',
+     ),
+     ),
+     ),
    floatingActionButton: FloatingActionButton(
      tooltip: 'Add',
      child: Icon(Icons.add),
-     onPressed: null,
+     onPressed: () {},
+     backgroundColor: Colors.deepOrange[600],
    ),
  ),
 ));
