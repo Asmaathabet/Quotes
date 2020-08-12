5cce1daf437c33e61ceab332b9be6399369ae02b
Asmaathabet <asmathabet2009@hotmail.com>
2020-08-12 18:39:00 +0300

    add assets - images to fultter app

--- lib/main.dart
+++ lib/main.dart
@@ -15,16 +15,22 @@ class Home extends StatelessWidget {
     backgroundColor: Colors.deepOrange[600],  // press ctrl + Q
     ),
     body: Center(
-    child: Text(
-    'This is Asmaa, Hello from Flutter',
-    style : TextStyle(
-    fontSize: 20.0,
-    fontWeight: FontWeight.bold,
-    letterSpacing: 0.2,
-    color: Colors.grey[600],
-    fontFamily: 'IndieFlower',
-    ),
-    ),
+//    child: Image.asset('assets/home.jpg'),
+      child: Image.network('https://images.unsplash.com/photo-1597207553648-5a63b420c157?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=306&q=80'),
+//    Text(
+//    'This is Asmaa, Hello from Flutter',
+//    style : TextStyle(
+//    fontSize: 20.0,
+//    fontWeight: FontWeight.bold,
+//    letterSpacing: 0.2,
+//    color: Colors.grey[600],
+//    fontFamily: 'IndieFlower',
+//    ),
+//    ),
+//     Image(
+////           image: NetworkImage('https://images.unsplash.com/photo-1597219833073-bfdf2bfe646f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'),
+////           image: AssetImage('assets/home.jpg'),
+//     )
     ),
     floatingActionButton: FloatingActionButton(
     tooltip: 'Add',
