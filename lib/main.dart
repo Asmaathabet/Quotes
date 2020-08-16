edbe2d7aaab18c93f308c4ade235024f948ab862
Asmaathabet <asmathabet2009@hotmail.com>
2020-08-16 23:50:17 +0300

    create new column & first field in it

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,30 @@ class myCard extends StatelessWidget {
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
       ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            Text(
+              'Asmaa Thabet',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
